e472bd8b6d0431543d824802f71c6de1ce737e4f
josiane299 <josianeyague@gmail.com>
2022-11-01 13:25:39 -0400

    showing current temp

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
 function displayTemperature(response) {
-  console.log (response.data.temperature.current);
+  console.log(response.data);
+  let temperatureElement = document.querySelector(`#temperature`);
+  temperatureElement.innerHTML = Math.round
+  (response.data.temperature.current);
+
+   let cityElement = document.querySelector(`#city`);
+   cityElement.innerHTML = response.data.city;
+
+
+   let descriptionElement = document.querySelector(`#description`);
+   descriptionElement.innerHTML = response.data.condition.description;
+
+   let humidityElement = document.querySelector(`#humidity`);
+   humidityElement.innerHTML = response.data.temperature.humidity;
+
+    let windElement = document.querySelector(`#wind`);
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+
 };
 
 
