68e0cc7f832156579eaa0fbdb5606d58f8ef0bf6
josiane299 <josianeyague@gmail.com>
2022-11-07 10:38:23 -0500

    adding the link to cities

--- src/app.js
+++ src/app.js
@@ -21,14 +21,14 @@ function formatDate(timestamp) {
   ];
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
-};
+}
 
 //temperature function
 
 function displayTemperature(response) {
-  console.log(response.data.temperature.current);
+  celsiusTemperature = response.data.temperature.current;
   let temperatureElement = document.querySelector(`#temperature`);
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 
   let cityElement = document.querySelector(`#city`);
   cityElement.innerHTML = response.data.city;
@@ -47,44 +47,64 @@ function displayTemperature(response) {
 
   let iconElement = document.querySelector(`#icon`);
   iconElement.setAttribute(`src`, response.data.condition.icon_url);
-  iconElement.setAttribute (`alt`, response.data.condition.description);
-
+  iconElement.setAttribute(`alt`, response.data.condition.description);
 }
 
-function search (city){
-let apiKey = "ad14tc2f8f3off39960b4fb3559c5c0a";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
-
-
+function search(city) {
+  let apiKey = "ad14tc2f8f3off39960b4fb3559c5c0a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-
-
-function handleSubmit (event) {
+function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector(`.search`);
   search(cityInputElement.value);
-  
-
 }
 
-function displayFehrenheitTemperature(event){
+function displayFehrenheitTemperature(event) {
   event.preventDefault();
-  let fahrenheitTemperature = (21 * 9)/5+32;
-  alert(fahrenheitTemperature);
-  
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  let temperatureElement = document.querySelector(`#temperature`);
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
-
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector(`#temperature`);
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
 
 //adding a search engine
 
-
-search (`Philadelphia`);
+let celsiusTemperature = null;
 
 let form = document.querySelector(`#search-form`);
 form.addEventListener(`submit`, handleSubmit);
 
 let fahrenheitLink = document.querySelector(`#fahrenheit-link`);
 fahrenheitLink.addEventListener(`click`, displayFehrenheitTemperature);
+
+let celsiusLink = document.querySelector(`#celsius-link`);
+celsiusLink.addEventListener(`click`, displayCelsiusTemperature);
+
+search(`Philadelphia`);
+
+//linking the cities 
+
+function inputTokyo(event){
+  console.log(showCities);
+  let showCities = document.querySelector(`#cities`);
+  showCities.innerHTML = "Tokyo";
+  let apiKey = "ad14tc2f8f3off39960b4fb3559c5c0a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Tokyo&key=${apiKey}&units=metric`;
+
+  celsiusLink.classList.add(`active`);
+  fahrenheitLink.classList.remove(`active`);
+  axios.get(apiUrl).then(displayTemperature);
+  
+}
+ 
+let showCities = document.querySelector(`#tokyo`);
+showCities.addEventListener(`click`,inputTokyo);
+
